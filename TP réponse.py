aca77675226788c8ff127d5d5e82dfd5cfbbaac7
Gibus <>
2021-11-10 07:57:14 +0000

    rien

--- TP réponse.py
+++ TP réponse.py
@@ -1,4 +1,47 @@
-## Exercice 1
+##A compiler avant (fonctions sur les listes chainées)
+class Cellule:
+    """ définit la classe Cellule"""
+    def __init__(self,v,s):
+        self.valeur = v
+        self.suivante = s
+
+    def __str__(self):
+        return str(self.valeur)
+
+def estVide(l):
+    return l==None
+
+
+def afficheListe(l):
+
+    res = '| '
+    while not estVide(l):
+        res+=str(l.valeur)+' '
+        l=l.suivante
+    return res+'|'
+
+def concatenerListe(l1,l2):
+
+    if l1 is None:
+        return l2
+    return Cellule(l1.valeur,concatenerListe(l1.suivante,l2))
+
+
+def reverse(l):
+    if l is None:
+        return None
+
+    res = None
+    while not l is None:
+
+        res = Cellule(l.valeur,res)
+        l=l.suivante
+
+    return res
+## Tri par selection avec tableaux dynamiques
+
+
+#Exercice 1
 
 def mini(l,nb):
   nb2 = 0
